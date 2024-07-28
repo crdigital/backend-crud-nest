c10179661e21b461fc54b2684e534fa374bd447a
Clayton Rodrigues <clayton.metal2@hotmail.com>
2024-07-28 14:44:35 -0300

    update build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from './db/db.module';
 import { ProdutoModule } from './produto/produto.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [DbModule, ProdutoModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
